bcfd2a67bc6283c6cc616f1815f8823610319d0b
MadCore <0xMadCore@gmail.com>
2026-05-19 23:26:03 +0300

    admk-ut : FEAT : add sys-admin-helpers(2)

--- sys-admin-helpers/switchbar.sh
+++ sys-admin-helpers/switchbar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -f /usr/bin/wf-panel-pi ]; then
-    mv /usr/bin/wf-panel-pi /usr/bin/wf-panel-pi.bak
-    pkill wf-panel-pi
+    sudo mv /usr/bin/wf-panel-pi /usr/bin/wf-panel-pi.bak
+    sudo pkill wf-panel-pi
 else
-    mv /usr/bin/wf-panel-pi.bak /usr/bin/wf-panel-pi
+    sudo mv /usr/bin/wf-panel-pi.bak /usr/bin/wf-panel-pi
 fi
--- sys-admin-helpers/update-helpers.sh
+++ sys-admin-helpers/update-helpers.sh
@@ -2,6 +2,10 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 OUTPUT_DIR=~/
 
+chmod +x $SCRIPT_DIR/help.sh
+chmod +x $SCRIPT_DIR/switchbar.sh
+chmod +x $SCRIPT_DIR/timefix.sh
+
 cp -f $SCRIPT_DIR/help.sh $OUTPUT_DIR/.
 cp -f $SCRIPT_DIR/switchbar.sh $OUTPUT_DIR/.
 cp -f $SCRIPT_DIR/timefix.sh $OUTPUT_DIR/.
